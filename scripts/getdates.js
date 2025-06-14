d77f4edf7ef7036fdb9740878744454e8730303f
Santiago1707 <dmendozaladino@byupathway.edu>
2025-06-14 18:34:31 -0400

    refactor: Remove unused JavaScript and CSS files

--- scripts/getdates.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Update copyright year
-    const currentYear = new Date().getFullYear();
-    document.getElementById('currentyear').textContent = currentYear;
-    
-    // Update last modified date
-    document.getElementById('lastModified').textContent = `Last Update: ${document.lastModified}`;
-}); 
